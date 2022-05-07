adbfe50d435e530218c1056fa6b5fd421dc55ac6
sunilpandey08 <44431108+sunilpandey08@users.noreply.github.com>
2022-05-07 21:14:32 +0530

    first commit

---
+++ get_url.py
@@ -0,0 +1,115 @@
+import sys,argparse
+
+try:
+    import mechanicalsoup
+    import requests
+except Exception as e:
+    print('mechanicalsoup package Not Installed\n')
+    print('type pip3 install mechanicalsoup')
+    exit()
+
+from lib import Parser
+
+urls = []
+
+class crawl(object):
+
+    auth = {
+        1:[
+            'https://www.google.com',
+            'class="r"><a href="/url\?q=(.*?)&amp',
+            'fl'
+        ],
+        2:[
+            'https://www.bing.com',
+            'h=".*?" href="(h.*?")',
+            "b_widePag sb_bp"
+        ]
+    }
+
+    def __init__(
+            self,
+            search_keyword):
+        self.search_keyword = search_keyword
+
+    def Bing(self):
+        bing = Parser(
+            self.search_keyword,
+            crawl.auth[2][0],
+            crawl.auth[2][1],
+            crawl.auth[2][2],
+        )
+        bing.request()
+        for url in dir(bing):
+            if 'microsoft.com' in url or 'bing.com' in url:
+                pass
+            else:
+                urls.append(url)
+
+    def Google(self):
+        google = Parser(
+            self.search_keyword,
+            crawl.auth[1][0],
+            crawl.auth[1][1],
+            crawl.auth[1][2],
+        )
+        google.request()
+        for url in dir(google):
+            if 'google.com' in url or 'google.co.in' in url:
+                pass
+            else:
+                urls.append(url)
+
+
+fel = sys.argv[0]
+par = argparse.ArgumentParser(
+    prog=fel,
+    usage="%(prog)s --search_keyword [keyword]",
+    formatter_class=argparse.RawTextHelpFormatter,
+    description="""Descriptions: Crawl Web Use Google search_keyword & Bing search_keyword""")
+
+par.add_argument('--search_keyword',
+                 help="""Your search_keyword e.g inurl:.php?id=""",
+                 metavar='[keywords]',
+                 type=str)
+arg = par.parse_args()
+
+try:
+    if arg.search_keyword != None:
+        _ = crawl(arg.search_keyword)
+        _.Bing()
+        _.Google()
+        if urls != []:
+            for url in list(set(urls)):
+                print('- {}'.format(url))
+        else:
+            print('\nNo Url Found !\n')
+    else:
+        par.print_help()
+    if arg.search_keyword != None:
+        _ = crawl(arg.search_keyword)
+        _.Bing()
+        _.Google()
+        if urls != []:
+            for url in list(set(urls)):
+                urlval = '{}\n'.format(url)
+                # Open a file with access mode 'a'
+                file_object = open('sample.txt', 'a')
+                # Append 'hello' at the end of file
+                file_object.write(urlval)
+                # Close the file
+                file_object.close()
+        else:
+            print('\nNo Url Found !\n')
+    else:
+        par.print_help()
+except Exception as e:
+    print(e)
+except KeyboardInterrupt:
+    exit()
+
+
+
+
+
+
---
+++ lib.py
@@ -0,0 +1,82 @@
+#-*- coding: utf-8 -*-
+
+from mechanicalsoup import StatefulBrowser
+from re import findall
+
+
+agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+
+
+class scrape(StatefulBrowser):
+
+      def __repr__(
+        fitur = {
+            'features':'html.parser'
+                 },
+        uag = agent
+      ):
+          return StatefulBrowser(
+            soup_config = fitur,
+            user_agent = agent
+          )
+
+class Parser(object):
+
+      __list = []
+
+      def __init__(
+        self,
+        search_keyword,
+        URL,
+        pattern,
+        class_tag
+      ):
+          self.search_keyword = search_keyword
+          self.URL = URL
+          self.__pattern = pattern
+          self.class_tag = class_tag
+          
+          
+      def __dir__(self):
+          return list(set(self.__list))
+
+          
+      def get_page(self):
+          self.__req = scrape()
+          s = self.__req.open(
+            self.URL,
+            timeout = 10
+          )
+          self.__req.select_form(
+            'form[action="/search"]'
+          )
+          self.__req['q'] = self.search_keyword
+          self.__req.submit_selected()
+          _content = str(self.__req.get_current_page())
+          for urls in findall(
+            self.__pattern,
+            _content                        
+          ):  
+              if 'www.google.com' in self.URL: self.__list.append(urls)
+              else: self.__list.append(urls[:-1])
+          return self.__req.get_current_page().find_all(
+            'a',
+            class_=self.class_tag
+          )
+          
+      def request(self):
+          self.__req = scrape()
+          for page in self.get_page():
+              try:
+                  self.__req.open(
+                    f'{self.URL}{page.get("href")}'
+                  )
+                  content = str(self.__req.get_current_page())
+                  for urls in findall(
+                  self.__pattern,
+                  content
+                  ):
+                      if 'www.google.com' in self.URL: self.__list.append(urls)
+                      else: self.__list.append(urls[:-1])
+              except Exception as e:
+                  print(e)
